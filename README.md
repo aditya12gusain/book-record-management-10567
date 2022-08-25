25f98999390aa28222f26f8149696147652c657e
aditya12gusain <adisandi3112002@gmail.com>
2022-08-25 21:27:45 +0530

    added new routes

--- README.md
+++ README.md
@@ -2,16 +2,20 @@
 
 This is a book record management API Backend for the management of records and books
 
+# API Documentation link
+
+https://documenter.getpostman.com/view/11423344/VUqyoZdU
+
 # Routes and Endpoints
 
 ## /users
 
-POST: Create a new user
-GET: Get all list of users
+POST: Create a new user ✅
+GET: Get all list of users ✅
 
 ## /users/{id}
 
-GET: Get a user by id
+GET: Get a user by id ✅
 PUT: Update a user by id
 DELETE: Delete a user by id (check if he/she still has an issued book) (is there any fine to be paid)
 
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+// JSON data import
+const { users } = require("./data/users.json");
 
 const app = express();
 
@@ -9,6 +11,108 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Server is up and running",
+  });
+});
+
+/**
+ * Route: /users
+ * Method: GET
+ * Description: Get all users
+ * Access: Public
+ * Parameters: none
+ */
+app.get("/users", (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: users,
+  });
+});
+
+/**
+ * Route: /users/:id
+ * Method: GET
+ * Description: Get single user by id
+ * Access: Public
+ * Parameters: id
+ */
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users.find((each) => each.id === id);
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User not found",
+    });
+  }
+  return res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+/**
+ * Route: /users
+ * Method: POST
+ * Description: Create new user
+ * Access: Public
+ * Parameters: none
+ */
+app.post("/users", (req, res) => {
+  const { id, name, surname, email, subscriptionType, subscriptionDate } =
+    req.body;
+
+  const user = users.find((each) => each.id === id);
+
+  if (user) {
+    return res.status(404).json({
+      success: false,
+      message: "User exists with this id",
+    });
+  }
+
+  users.push({
+    id,
+    name,
+    surname,
+    email,
+    subscriptionType,
+    subscriptionDate,
+  });
+  return res.status(201).json({
+    success: true,
+    data: users,
+  });
+});
+
+/**
+ * Route: /users/:id
+ * Method: PUT
+ * Description: Updating user data
+ * Access: Public
+ * Parameters: id
+ */
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { data } = req.body;
+
+  const user = users.find((each) => each.id === id);
+
+  if (!user)
+    return res.status(404).json({ success: false, message: "User not found" });
+
+  const updatedUser = users.map((each) => {
+    if (each.id === id) {
+      return {
+        ...each,
+        ...data,
+      };
+    }
+    return each;
+  });
+
+  return res.status(200).json({
+    success: true,
+    data: updatedUser,
   });
 });
 
